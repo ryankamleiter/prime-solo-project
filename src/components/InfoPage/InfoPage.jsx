4b4a50e9b6e0c9a0ae36049750be94f4f1f8b087
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-03-15 11:04:54 -0500

    added info page

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -8,7 +8,7 @@ import React from 'react';
 function InfoPage() {
   return (
     <div className="container">
-      <p>Info Page</p>
+      <p>Welcome to my Sports Card Tracking Application, where users can effortlessly manage their sports card collections. This app offers a seamless experience with features including collection management, wishlist functionality, and transaction history tracking. Easily organize and view your cards, set up a wishlist to track desired additions, and monitor the buying and selling activity of your collection. With a user-friendly interface and customizable options our app caters to collectors of all levels.</p>
     </div>
   );
 }
